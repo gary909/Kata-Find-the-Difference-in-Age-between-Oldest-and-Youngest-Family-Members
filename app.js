bdcdfcc6dc3b29ee73bc25eaea7f073186d6924b
gary909 <garywhite61@hotmail.com>
2022-10-20 13:16:20 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function differenceInAges(ages) {
-
+    let min = Math.min.apply(null, ages);
+    let max = Math.max.apply(null, ages);
+    let diff = max - min;
+    let myArr = [min, max, diff];
+    return myArr;
 }
 
 console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
